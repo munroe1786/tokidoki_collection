250bd53e3ef8c44fb77eb5b90ecee79e7030d7e9
munroe1786 <era3munroe@gmail.com>
2020-04-05 13:33:50 -0500

    edit seed to reflect change to from series to family, edit index.js to reflect change from series to family

--- tokidoki_api/db/seeds.rb
+++ tokidoki_api/db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-series = Series.create([{ number: 1}, { number: 2}, { number: 3}, { number: 4}, { number: 5}, { number: 6}, { number: 7}, { number: 8}])
+family = Family.create([{ name: "Unicorno"}, { name: "Mermicorno"}, { name: "Cactus Friends"}, { name: "Donutella and Her Sweet Friends"}, { name: "Punkstar"}, { name: "Til Death Do Us Part"}])
 
-Character.create(name: "Stellina", family: "Unicorno", description: "First Unicorno released. White with 2 red stars and 1 yellow star, with a rainbow mane.", release_year: "2012", photo_url: "https://media.dollskill.com/media/EDr3wJyNepygzKGzrgQEPJBSFulm5ttX-34.jpg", series_id: 1)
+Character.create(name: "Stellina", description: "First Unicorno released. White with 2 red stars and 1 yellow star, with a rainbow mane.", series: "1", release_year: "2012", photo_url: "https://media.dollskill.com/media/EDr3wJyNepygzKGzrgQEPJBSFulm5ttX-34.jpg", family_id: 1)
 
-Character.create(name: "Sharkbite", family: "Mermicorno", description: "Light grey body with darker grey tail and mane, shark image on his side and shark tooth necklace", release_year: "2017", photo_url: "https://s3.amazonaws.com/trampt/images/products/000/301/699/Mermicorno_-_Sharkbite-Tokidoki_Simone_Legno-Mermicorno-Tokidoki-trampt-301699o.jpg?1550374381", series_id: 2)
+Character.create(name: "Sharkbite", description: "Light grey body with darker grey tail and mane, shark image on his side and shark tooth necklace", series: "2", release_year: "2017", photo_url: "https://s3.amazonaws.com/trampt/images/products/000/301/699/Mermicorno_-_Sharkbite-Tokidoki_Simone_Legno-Mermicorno-Tokidoki-trampt-301699o.jpg?1550374381", family_id: 2)
