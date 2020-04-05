bbfcbda9336265cae697e3776156d5569120cf05
munroe1786 <era3munroe@gmail.com>
2020-04-05 11:16:50 -0500

    delete character class and render card function from index.js and move it to character.js, add js script tags for character.js and charactersPage.js to index.html, move all script tags to bottom of body of index.html

---
+++ tokidoki_client/js/Character.js
@@ -0,0 +1,33 @@
+class Character {
+    constructor({ id, name, family, description, release_year, photo_url, series_id }) {
+        this.id = id
+        this.name = name
+        this.family = family
+        this.description = description
+        this.release_year = release_year
+        this.photo_url = photo_url
+        this.series_id = series_id
+    }
+
+    photoHtml() {
+        return `<img src="${this.photo_url}" />`
+    }  
+
+    renderCard() {
+        let article = document.createElement('article')
+        article.class = "fl w-100 w-50-m w-25-ns pa2-ns"
+        article.dataset['character_id'] = this.id
+        article.innerHTML = `
+            <div class="aspect-ratio aspect-ratio--1x1">
+                <img style="background-image:url(${this.photo_url});"
+                class="db bg-center cover aspect-ratio--object" />
+            </div>
+            <a href="#0" class="ph2 ph0-ns pb3 link db">
+                <h3 class="f5 f4-ns mb0 black-90">${this.name}</h3>
+                <h3 class="f6 f5 fw4 mt2 black-60">${this.family}</h3>
+            </a>
+            <p><button class="editCharacter" data-id="${this.id}">Edit Character</button></p>
+            `
+        return article
+    }
+}
