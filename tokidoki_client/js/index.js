68969e1629bcd2680dd1c6336867965d424679e9
munroe1786 <era3munroe@gmail.com>
2020-04-05 13:34:26 -0500

    edit index.js to reflect change from series to family

--- tokidoki_client/js/index.js
+++ tokidoki_client/js/index.js
@@ -7,7 +7,7 @@ let stellina = new Character({
     id: 1,
     name: "Stellina",
     description: "Stars and a rainbow mane",
-    series: "1"
+    series: "1",
     release_year: "2012",
     photo_url: "https://dtpmhvbsmffsz.cloudfront.net/posts/2016/02/08/56b8d59df0137d854204dd16/m_56b8d59df0137d854204dd17.jpg",
     family_id: 1
