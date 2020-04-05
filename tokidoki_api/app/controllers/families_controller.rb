56b05e323ef149854a83ca28db6e6267c56bc63b
munroe1786 <era3munroe@gmail.com>
2020-04-05 14:44:13 -0500

    add before action, show action and private methods to families controller

--- tokidoki_api/app/controllers/families_controller.rb
+++ tokidoki_api/app/controllers/families_controller.rb
@@ -1,6 +1,22 @@
 class FamiliesController < ApplicationController
+    before_action :set_family, only: [:show, :update, :destroy]
+
     def index
-        families = Family.all 
-        render json: families
+        @families = Family.all 
+        render json: @families
     end
+
+    def show
+        render json: FamilySerializer.new(@family).serialized_json
+    end
+
+    private
+
+        def set_family
+            @family = Family.find(params[:id])
+        end
+
+        def family_params
+            params.require(:family).permit(:name, :photo_url)
+        end
 end
