114c0a3c9aadf62911c3c13fb27fd2dc8240e093
munroe1786 <era3munroe@gmail.com>
2020-04-05 11:32:41 -0500

    add constructor and renderForm function to charactersPage.js

--- tokidoki_client/js/CharactersPage.js
+++ tokidoki_client/js/CharactersPage.js
@@ -1,4 +1,32 @@
 class CharactersPage {
+    
+    constructor() {
+        this.characters = []
+        this.formState = {
+            name: '',
+            family: ''
+        }
+    }
+
+    renderForm() {
+        return `
+            <form class="addCharacter">
+                <p>
+                    <label>Name</label>
+                    <input type="text" name="name" value="${this.formState.name}" />
+                </p>
+                <p>
+                    <label>Family</label>
+                    <input type="text" name="family" value="${this.formState.family}" />
+                </p>
+                <input type="submit" value="Add Character" />
+            </form>
+        `
+    }
+
+    
+    
+    
     render() {
         return `
         <h1>Hello from Characters Page</h1>
