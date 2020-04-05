f1c5bc364760ebc6012a7c6f50a2af535f8f5c85
munroe1786 <era3munroe@gmail.com>
2020-04-05 10:33:26 -0500

    add destructuring to character constructor, add photoHtml method in character class

--- tokidoki_client/js/index.js
+++ tokidoki_client/js/index.js
@@ -1,7 +1,15 @@
 class Character {
-    constructor(attributes) {
-        const { id, name, family, description, release_year, photo_url, series_id } 
-        = attributes
-        debugger
+    constructor({ id, name, family, description, release_year, photo_url, series_id }) {
+        this.id = id
+        this.name = name
+        this.family = family
+        this.description = description
+        this.release_year = release_year
+        this.photo_url = photo_url
+        this.series_id = series_id
     }
+    photoHtml() {
+        return `<img src="${this.photo_url}" />`
+    }    
+
 }
