96f32075366e14115fd56bce666cf739d13de8b8
munroe1786 <era3munroe@gmail.com>
2020-04-05 10:10:29 -0500

    add character class to index.js, add debugger to index.html

---
+++ tokidoki_client/js/index.js
@@ -0,0 +1,7 @@
+class Character {
+    constructor(attributes) {
+        const { id, name, family, description, release_year, photo_url, series_id } 
+        = attributes
+        debugger
+    }
+}
