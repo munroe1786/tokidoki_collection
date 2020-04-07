63539157cdc8e92b8af3dc2eafb733c2b4c0b34f
munroe1786 <era3munroe@gmail.com>
2020-04-07 16:07:43 -0500

    add before action and show action to characters controller / edit characters index action to have instance variables

--- tokidoki_api/app/controllers/characters_controller.rb
+++ tokidoki_api/app/controllers/characters_controller.rb
@@ -1,6 +1,12 @@
 class CharactersController < ApplicationController
+    before_action :set_character, only: [:show, :update, :destroy]
+    
     def index
-        characters = Character.all 
-        render json: characters
+        @characters = Character.all 
+        render json: @characters
+    end
+
+    def show
+        render json: @character
     end
 end
