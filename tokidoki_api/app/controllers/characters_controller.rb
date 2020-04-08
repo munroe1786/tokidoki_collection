1a22e83829465c9af8cc25497ea378dd566e2a4f
munroe1786 <era3munroe@gmail.com>
2020-04-08 16:50:56 -0500

    add character serializer to character index action / build out character show and create actions / add private methods and strong params

--- tokidoki_api/app/controllers/characters_controller.rb
+++ tokidoki_api/app/controllers/characters_controller.rb
@@ -3,10 +3,32 @@ class CharactersController < ApplicationController
     
     def index
         @characters = Character.all 
-        render json: @characters
+        render json: CharacterSerializer.new(@character).serialized_json
     end
 
     def show
-        render json: @character
+        family = Family.find(params[:family_id])
+        if family
+            render json: CharacterSerializer.new(@character).serialized_json
+        end
     end
+
+    def create
+        @character = Character.new(character_params)
+        if @character.save
+            render json: @character, status: :created, location: @character
+        else
+            render json: @character.errors, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+        def set_character
+            @character = Character.find(params[:id])
+        end
+
+        def character_params
+            params.require(:character).permit(:name, :description, :series, :release_year, :photo_url, :family_id)
+        end
 end
