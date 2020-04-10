ece9869d27ef880d7def4dd68a343383920aeb0f
munroe1786 <era3munroe@gmail.com>
2020-04-09 20:55:16 -0500

    delete previous js files --they have been condensed into tokidoki.js / delete css file / delete notes file

--- tokidoki_client/js/FamilyAPI.js
+++
@@ -1,7 +0,0 @@
-class FamilyAPI {
-    static getFamilies() {
-        return fetch(`${FamilyAPI.base_url}/families`).then(res => res.json())
-    }
-}
-
-FamilyAPI.base_url = "http://localhost:3000"
